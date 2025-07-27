37d6e52babf6b08db9b1a00377314a73043e1b20
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2025-07-27 14:37:02 +0530

    Update amplify_config.js

--- js/amplify_config.js
+++ js/amplify_config.js
@@ -1,1 +1,10 @@
+const awsExports = {
+  Auth: {
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_RMhlKuGHS',
+    userPoolWebClientId: '4g0bo9ac68vq4g817v1je4e5sf',
+    authenticationFlowType: 'USER_PASSWORD_AUTH'
+  }
+};
+export default awsExports;
 
